376819bc741bc331500bb7cd18e679a591950e58
yuyoujiang <254693273@qq.com>
2024-12-31 18:08:30 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,20 +10,21 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Elaine Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://elainedanwu.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'elainedanwu', // Usually your GitHub org/user name.
+  projectName: 'elainedanwu.github.io', // Usually your repo name.
+  deploymentBranch: 'gh_pages',  // Add by youjiang
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
